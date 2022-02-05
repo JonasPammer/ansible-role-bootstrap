95de463123e15fe0d414e7274ab0fef7230d0712
Jonas Pammer <opensource@jonaspammer.at>
2022-02-05 14:56:46 +0100

    ci: use commitlint conventional configuration
    i.e., adds "chore" as a type and adds body constrictions

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: [
-    "@commitlint/config-angular", // scoped packages are not prefixed
+    "@commitlint/config-conventional", // scoped packages are not prefixed
   ],
   rules: {
     "header-max-length": [2, "always", 72 * 2],
