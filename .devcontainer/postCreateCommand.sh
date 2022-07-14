de96e88ca645ff19b30b88b073a3083e88e80046
Jonas Pammer <opensource@jonaspammer.at>
2022-07-14 14:26:50 +0000

    refactor: use postCreateCommand instead of manually altering .bashrc/zshrc
    results in better name of file and less hacky as this is a feature of devcontainer.
    without /bin/bash -ic (interactive!) it would be /bin/sh and without things brought to by bash-it/bashrc

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,3 @@
+
+pre-commit install --install-hooks
+#pre-commit install --hook-type commit-msg
--- .devcontainer/shrc-addition.sh
+++
@@ -1,3 +0,0 @@
-# This file is copied to the end of .zshrc and .bashrc
-pre-commit install --install-hooks
-pre-commit install --hook-type commit-msg
